ebce82cc42b0a21f0b8fdd0072526f6b56d30eb5
Technical-Support-1 <80965507+Technical-Support-1@users.noreply.github.com>
2021-05-21 23:19:48 +0800

    差不多做完了（修复了BUG，加了注释）

--- a001.py
+++ a001.py
@@ -5,19 +5,19 @@ panda_name = input("请输入表格名称（须和代码文件在同一目录下
 def y_1(x):
     try:
         y = pd.read_excel(x)  # 读取数据来检测
-        return True
+        return True   # 正确则返回True
     except FileNotFoundError:
-        return False
+        return False   #  查询不到否则返回False
 if y_1(panda_name):
-    nnn = 10
+    nnn = 10   # no ture thing
 else:
     print("文件名错误，请重试！")
     while True:
         panda_name = input("请输入表格名称（须和代码文件在同一目录下，带上后缀名）：") # 获取表格名称
         if y_1(panda_name):
-            break
+            break   # 如果成功就跳出循环
         else:
-            print("文件名错误，请重试！")        
+            print("文件名错误，请重试！")        #  否则继续循环
 data = pd.read_excel(panda_name)
 data1 = data.head(2)   #　起始行（取一行作为value，单学科仅需一个成绩）
 data2 = data1.drop(data1.index[[0]],axis=0)   # 删除不必要的行数
@@ -67,26 +67,33 @@ while True:
                 mark = float(mark1)
             else:
                 print("请输入正确的数字！")
-                k_a = 3   #  为了防止错误播报成功消息
-                subject = 564984   # 检查是否为数字或浮点数
-        for a_55 in Chinese:
-            if a_55 == name:                
-                if subject == "Chinese":
-                    Chinese[name] = mark
-                    k_a = 5
-                elif subject == "Maths":
-                    Maths[name] = mark
-                    k_a = 5
-                elif subject == "English":
-                    English[name] = mark
-                    k_a = 5
-                else:
-                    if subject != 564984:      # 检查是否为数字或浮点数
-                        print("请输入正确的科目！")       
-                    k_a = 3
-                if k_a == 5:
-                    print("成功！")
-                
+                k_a = 3   #为了防止错误播报成功消息
+                subject = 564984   # 防止语句错误执行
+                # 以上代码是为了检测输入是否为整数或浮点数，下同
+
+        if subject != "Chinese":
+            if subject != "Maths":
+                if subject != "English":
+                    print("请输入正确的科目！")
+                    k_a = 3     # 判断是否输入正确的科目
+        if subject == "Chinese":
+            Chinese[name] = mark
+            Maths[name] = "nan"     # 覆盖性赋值
+            English[name] = "nan"  # 防止出现访问错误
+            k_a = 5     # 中心语句（此行是为了防止错误播报成功消息，下同）
+        elif subject == "Maths":
+            Maths[name] = mark
+            Chinese[name] = "nan"
+            English[name] = "nan"
+            k_a = 5
+        elif subject == "English":
+            English[name] = mark
+            Chinese[name] = "nan"
+            Maths[name] = "nan"
+            k_a = 5
+        if k_a == 5:
+            print("成功！")    # 播报成功消息
+
 
     if act == "d":
         name = input("请输入人名：")
@@ -99,14 +106,14 @@ while True:
             k = 3
         if k !=5:
             print("人名不存在，请重试")           # 检查是否存在此人（下同）
-            subject = 0
+            subject = 0    # 防止执行后面的语句
         if subject == "Chinese":
-            d_test=input(F"TA的分数是{Chinese[name]},您确定要继续吗？（Y/N）")
+            d_test=input(F"TA的分数是{Chinese[name]},您确定要继续吗？（Y/N）")   # 确认信息
             if d_test == "Y":
-                Chinese.pop(name)
+                Chinese.pop(name)   # 删除成绩
                 print("成功！")
             else:
-                print("操作已取消")
+                print("操作已取消")    #  否则取消操作
         elif subject == "Maths":
             d_test=input(F"TA的分数是{Maths[name]},您确定要继续吗？（Y/N）")
             if d_test == "Y":
@@ -122,7 +129,7 @@ while True:
             else:
                 print("操作已取消")
         else:
-            print("请输入正确的科目！")
+            print("请输入正确的科目！")  # 检测是否是正确的科目
 
     if act == "c":
         name_c = input("请输入人名：")
@@ -135,7 +142,7 @@ while True:
         else:
             k = 3
         if k != 5:
-            print("人名不存在，请重试!")
+            print("人名不存在，请重试!")   # 检测人名是否存在
         if mark_h.isnumeric():
             mark_c = float(mark_h)
         else:
@@ -145,10 +152,10 @@ while True:
                 mark_c = float(mark_h)
             else:
                 print("请输入正确的数字！")
-                subject_1 = 46546846
-        if subject_1 == "Chinese":
-            Chinese[name_c] = mark_c
-            print("成功修改！")
+                subject_1 = 46546846  # 确保是整数或浮点数（此行是为了防止执行后面的语句）
+        if subject_1 == "Chinese":   # 判断科目
+            Chinese[name_c] = mark_c   #  核心语句（赋值）
+            print("成功修改！")   # 返回结果（下同）
         if subject_1 == "Maths":
             Maths[name_c] = mark_c
             print("成功修改！")
@@ -159,7 +166,7 @@ while True:
             if subject_1 != "Maths":
                 if subject_1 != "English":
                     if subject_1 != 46546846:
-                        print("请输入正确的科目！")
+                        print("请输入正确的科目！")   # 检测是否输入正确的科目
 
     if act == "f":
         test_f = input("您想要以分数还是人名或科目查询？(M/N/S):")
@@ -176,22 +183,22 @@ while True:
                     test_cg = 5
                 else:
                     print("请输入正确的数字！")
-                    test_cg = 3
-                if test_cg == 5:
-                    mark_f = float(mark_cg)
-                    list_f = ["语文"]
-                    for i in Chinese:
-                        if mark_f == Chinese[i]:
-                            list_f.append(i)
-                    list_f.append("数学")
-                    for i in Maths:
-                        if mark_f == Maths[i]:
-                            list_f.append(i)
-                    list_f.append("英语")
-                    for i in English:
-                        if mark_f == English[i]:
-                            list_f.append(i)
-                    print("查询到的人：",list_f)
+                    test_cg = 3     # 确保是整数或浮点数（此行是为了防止执行后面的语句）
+            if test_cg == 5:
+                mark_f = float(mark_cg)
+                list_f = ["语文"]   # 首先添加便于标识，下同
+                for i in Chinese:
+                    if mark_f == Chinese[i]:   #  如果符合，则添加相应元素
+                        list_f.append(i)
+                list_f.append("数学")
+                for i in Maths:
+                    if mark_f == Maths[i]:
+                        list_f.append(i)
+                list_f.append("英语")
+                for i in English:
+                    if mark_f == English[i]:
+                        list_f.append(i)
+                print("查询到的人：",list_f)
         if test_f == "N":
             name_f = input("请输入姓名：")
             for i in Chinese:
@@ -201,14 +208,14 @@ while True:
             else:
                 k = 3
             if k != 5:
-                print("人名不存在，请重试!")                
+                print("人名不存在，请重试!")             #  检测此人是否存在
             if k == 5:
                 print(F"{name_f}的语文成绩是{Chinese[name_f]}，数学成绩是{Maths[name_f]}，英语成绩是{English[name_f]}")
         if test_f == "S":
             subject_f = input("请输入学科名称：")
             if subject_f == "Chinese":
                 for f in Chinese:
-                        print(F"{f}，{Chinese[f]}分")
+                        print(F"{f}，{Chinese[f]}分")  #先遍历字典得到value，再用value得到key
             if subject_f == "Maths":
                 for f in Maths:
                         print(F"{f}，{Maths[f]}分")
@@ -218,19 +225,23 @@ while True:
             if subject_f != "Chinese":
                 if subject_f != "Maths":
                     if subject_f != "English":
-                        print("请输入正确的科目！")
+                        print("请输入正确的科目！")   # 判断是否输入了正确的科目
     if act == "break":
-        break
+        break      #   检测使用
 
     if act == "Export":
         ex_ch = input("请输入Excel表格名称：")
         k = 3
-        test_last = []
-        test_last = ex_ch.split(".")
-        if test_last[len(test_last)-1] != "xlsx":
-            print("文件名错误，请重试！")
-            k = 5
-        
+        test_last = ex_ch.split(".")   #  用.分割文件名（没有.则直接输出只有1个元素的列表）
+        if ex_ch == "":
+            ex_ch = time.strftime('%Y-%m-%d')   #  没有输入则用时间输出
+            test_last_1 = ".xlsx"
+            ex_ch = F"{ex_ch}{test_last_1}"    # 接上一个后缀名
+        else:
+            if test_last[len(test_last)-1] != "xlsx":
+                print("文件名错误，请重试！")
+                k = 5   #  如果既不为空也不以.xlsx结尾，则重新输入
+                
         Chinese.update({'成绩\姓名':'语文'})
         Maths.update({'成绩\姓名':'数学'})
         English.update({'成绩\姓名':'英语'})
@@ -245,15 +256,15 @@ while True:
                 title = list(Maths)
             if len(Maths) < len(English):
                 title = list(English)
-        title = list(Chinese)
+        title = list(Chinese)     #  以上是为了判断用哪个字典获取人名
 
-        title.insert(0,title.pop())
+        title.insert(0,title.pop())  
         table = pd.DataFrame()
         table = table.append(Chinese,ignore_index=True)
         table = table.append(Maths,ignore_index=True)
         table = table.append(English,ignore_index=True)
         
         table = table[title]
-        if k != 5:
+        if k != 5:    # 执行正确的语句
             table.to_excel(ex_ch,index=False,header=True)
             print("成功！")
